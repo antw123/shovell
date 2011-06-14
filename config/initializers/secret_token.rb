44353c1627deaa8d015cee5ff1a0f6cea0c00b2b
Anthony Williams <anthony@madebypurple.com>
2011-06-14 11:10:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shovell::Application.config.secret_token = '8b38bf619ab18356347efe046f00d34cd09ef40e976dcf27a96ffd7709d0f7fcc7af7f021b9e999588625cf8048e971a626622b8f25dca70c6c81fb9491098f4'
